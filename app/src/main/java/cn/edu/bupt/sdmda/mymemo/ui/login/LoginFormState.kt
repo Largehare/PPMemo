06afdb8e338f4ce8cec535bf3137ae40bd2dcc29
Largehare <chengg626@gmail.com>
2021-12-10 18:57:15 +0800

    添加login

---
+++ app/src/main/java/cn/edu/bupt/sdmda/mymemo/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package cn.edu.bupt.sdmda.mymemo.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val usernameError: Int? = null,
+                      val passwordError: Int? = null,
+                      val isDataValid: Boolean = false)
---
+++ app/src/main/java/cn/edu/bupt/sdmda/mymemo/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package cn.edu.bupt.sdmda.mymemo.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult (
+     val success:LoggedInUserView? = null,
+     val error:Int? = null
+)
